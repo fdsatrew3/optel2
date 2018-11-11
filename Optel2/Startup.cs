7932ec8ddbe1b05ae7d0245731d1f58e1a16bc4f
fdsatrew <fdsatrewq3@gmail.com>
2018-11-11 12:46:45 +0300

    Добавьте файлы проекта.

---
+++ Optel2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Optel2.Startup))]
+namespace Optel2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
