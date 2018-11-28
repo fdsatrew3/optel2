e69175ac28bde698d65ca4255b34fc2b6c7b5c5b
fdsatrew <fdsatrewq3@gmail.com>
2018-11-28 09:53:51 +0300

    - Added classes for both algorithms

---
+++ Optel2/Models/SliceLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optel2.Models
+{
+    public class SliceLine
+    {
+        // TBD
+    }
+}
