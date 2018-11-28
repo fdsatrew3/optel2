94d59aba6e6a678bd41ffda81a9a787b99722ead
fdsatrew <fdsatrewq3@gmail.com>
2018-11-28 09:53:51 +0300

    - Added classes for both algorithms

---
+++ Optel2/Algorithms/ExecutionTimeAndCost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class ExecutionTimeAndCost
+    {
+        public decimal ExecutionTime;
+        public decimal ExecutionCost;
+        public decimal RetargetingTime;
+        public DateTime ExecutionStart;
+        public DateTime ExecutionEnd;
+    }
+}
